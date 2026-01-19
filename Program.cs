7cfc316602c6ba980544144cdb849460d7b7569b
Md Abu Rayhan <contact.mdaburayhan@gmail.com>
2026-01-20 04:20:04 +0600

    update api

--- Program.cs
+++ Program.cs
@@ -81,16 +81,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "RealProxy API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RealProxy API V1");
+});
 
 app.UseHttpsRedirection();
 
@@ -100,6 +96,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Redirect root URL to Swagger
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 
 app.Run();
 
